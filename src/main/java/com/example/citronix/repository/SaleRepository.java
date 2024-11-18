bd4d2982807e7012ee2281ea23f12719a65970db
ali <fiharali0@gmail.com>
2024-11-18 16:08:34 +0100

    git error

--- src/main/java/com/example/citronix/repository/SaleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.citronix.repository;
-
-import com.example.citronix.model.Sale;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface SaleRepository extends JpaRepository<Sale , UUID> {
-}
--- src/main/java/com/example/citronix/web/errors/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.citronix.web.errors.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
